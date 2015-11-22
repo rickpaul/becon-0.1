1844133b2d771c2f2b123d5f9229ff1c04b6d548
Rickpaul Dhindsa <rickpaul.dhindsa@gmail.com>
2015-11-22 18:14:37 -0500

    Revert "initial commit"
    This reverts commit c93b388d0117ae54828d6740f1238673492ef035.

--- EMF_Logging_lib.py
+++
@@ -1,7 +0,0 @@
-from EMF_lib import HomeDirectory
-
-######################## LOGGING CODE
-LogRepository = HomeDirectory + 'log/'
-TestLogFilePath = LogRepository + 'test.log'
-DeletableLogFilePath = LogRepository + 'deletableLog.log'
-defaultLog = DeletableLogFilePath
