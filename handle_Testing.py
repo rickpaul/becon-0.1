a427a5b3c2f09c09877cab11ae635820c18945cd
rickpaul <rickpaul@Rickpauls-MacBook-Air-2.local>
2015-12-19 11:12:52 -0500

    Implementing Logging Handle

--- handle_Testing.py
+++ handle_Testing.py
@@ -1,4 +1,5 @@
 # EMF 		From...Import
+from handle_Logging		 	import EMF_Logging_Handle
 from handle_DataSeries	 	import EMF_DataSeries_Handle
 from handle_WordSeries 		import EMF_WordSeries_Handle
 from lib_EMF		 		import TEMP_MODE
@@ -7,22 +8,17 @@ from util_Logging	 		import init_logging
 # EMF 		Import...As
 # System 	Import...As
 import logging 	as log
-# System 	From...Import
-from os import remove
 
 class EMF_Testing_Handle:
 	def __init__(self, mode=TEMP_MODE):
 		(self.logLoc, self.deleteLog) = init_logging(mode=mode)
 		self.hndl_DB = create_DB(mode=mode)
+		self.hndl_Log = EMF_Logging_Handle(mode=mode)
 		self.wordHandles = {}
 
 	def __del__(self):
 		del self.hndl_DB
-		if self.deleteLog:
-			log.warning('Log File {} deleted'.format(self.logLoc))
-			remove(self.logLoc)
-		else:
-			log.info('Log File stored in {}'.format(self.logLoc))
+		del self.hndl_Log
 
 	def insert_test_data(self, dates, dataSet, dataTickers):
 		dataHandle = EMF_DataSeries_Handle(self.hndl_DB)
@@ -32,13 +28,11 @@ class EMF_Testing_Handle:
 			except IndexError:
 				values = dataSet
 			dataHandle.set_data_series(name=ticker, ticker=ticker, insertIfNot=True)
-			dataHandle.insert_data_history(dates, values)
+			dataHandle.write_to_DB(dates, values)
 			dataHandle.unset_data_series()
 
 	def create_word_from_data(self, dataTicker, transformationPattern):
-		wordHandle = EMF_WordSeries_Handle(self.hndl_DB, transformationPattern)
-		dataHandle = EMF_DataSeries_Handle(self.hndl_DB)
-		wordHandle.set_data_series(ticker=dataTicker)
+		wordHandle = EMF_WordSeries_Handle(self.hndl_DB, dataTicker, transformationPattern)
 		self.wordHandles[str(wordHandle)] = wordHandle
 		return str(wordHandle)
 
