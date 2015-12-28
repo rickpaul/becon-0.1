287615f4307101237303633e2175aff0f93b3613
rickpaul <rickpaul@Rickpauls-MacBook-Air-2.local>
2015-12-28 11:46:14 -0500

    date time cleanup
    —

--- lib_DataSeries.py
+++ lib_DataSeries.py
@@ -6,11 +6,3 @@ DATE_COL_dtype = (DATE_COL, 'int')
 VALUE_COL_dtype = (VALUE_COL, 'float64')
 DATA_HISTORY_DTYPE = [DATE_COL_dtype, VALUE_COL_dtype]
 
-Periodicity = {
-	'daily' 	: 	365,
-	'weekly'	:	52,
-	'monthly'	:	12,
-	'quarterly'	:	4,
-	'annually'	:	1,
-}
-
