111e9295947f3239082edd763233a5a7e0ba3084
rickpaul <rickpaul@Rickpauls-MacBook-Air-2.local>
2016-02-03 11:57:08 -0500

    Additions to Model
    + Added OLS Model
    + Added model_ID for unique model identification (to use in stats db)

--- lib_Model.py
+++ lib_Model.py
@@ -1,6 +1,7 @@
 # TODO:
-#	can we make kwargs functions? 
+#	can we make kwargs functions? We want to pass in dynamic values
 #		(suppose we want to get 2% of data set size for min_samples_split)
+# 	We're not using kwargDefaults
 #	VERIFY THAT DEC TREE CAN ACCEPT CATEGORICAL VARIABLES!
 
 # EMF 		From...Import
@@ -8,7 +9,7 @@ from template_Model 	import EMF_Model_Template
 from handle_Model 		import EMF_Model_Handle
 # System 	From...Import
 from math 				import log
-
+from string 			import join
 kwargDefaults = {
 	'max_depth':			3,
 	'min_samples_split':	10,
@@ -23,6 +24,7 @@ kwargDefaults = {
 # Good results are often achieved when setting max_depth=None in combination with min_samples_split=1 
 # The relative rank (i.e. depth) of a feature used as a decision node in a tree can be used to assess the relative importance of that feature with respect to the predictability of the target variable.
 EMF_ClassificationDecisionTree_Info = {
+	'model_ID': 2,
 	'model_short_name': 'ClassTree',
 	'supervised': True,
 	'computational_cost_construction': lambda numSamples, numFeatures: (numSamples*log(numSamples)*numFeatures),
@@ -51,6 +53,7 @@ class EMF_ClassificationDecisionTree(EMF_Model_Handle, EMF_Model_Template):
 		self.model = DecisionTreeClassifier(**self.kwargs)
 
 EMF_RegressionDecisionTree_Info = {
+	'model_ID': 1,
 	'model_short_name': 'RegrTree',
 	'supervised': True,
 	# 'computational_cost_construction': lambda numSamples, numFeatures: (numSamples*log(numSamples)*numFeatures),
@@ -80,6 +83,34 @@ class EMF_RegressionDecisionTree(EMF_Model_Handle, EMF_Model_Template):
 		from sklearn.tree import DecisionTreeRegressor
 		self.model = DecisionTreeRegressor(**self.kwargs)
 
+############# LINEAR REGRESSION
+
+EMF_OLSRegression_Info = {
+	'model_ID': 3,
+	'model_short_name': 'RegrTree',
+	'supervised': True,
+	# 'computational_cost_construction': lambda numSamples, numFeatures: (numSamples*log(numSamples)*numFeatures),
+	# 'computational_cost_query': lambda numSamples, numFeatures: (log(numSamples)),
+	'allowed_predVar_types': [
+		'categorical_unbounded', 
+		'categorical_bounded',
+		'continuous'
+	],
+	'allowed_respVar_types': [
+		'continuous',
+	],
+	'kwargs': {},
+}
+class EMF_OLSRegression(EMF_Model_Handle, EMF_Model_Template):
+	'''
+	http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html
+	'''
+	def __init__(self, hndl_WordSet):
+		self.__dict__ = EMF_OLSRegression_Info
+		super(EMF_OLSRegression, self).__init__(hndl_WordSet)
+		from sklearn.linear_model import LinearRegression
+		self.model = LinearRegression(**self.kwargs)
+
 AvailableModels = {
 	'BCM': None,
 	'ClassDecisionTree': EMF_ClassificationDecisionTree,
@@ -87,5 +118,9 @@ AvailableModels = {
 	'RandomForest':	None,
 	'ExtraTrees':	None,
 	'Bayesian': None,
-	'LinearRegression': None,
+	'LinearRegression': EMF_OLSRegression,
+	'ARDRegression': None,
+	'BayesianRidgeRegression': None,
+	'RANSACRegression': None,
+	'TheilSenRegression': None,
 }
